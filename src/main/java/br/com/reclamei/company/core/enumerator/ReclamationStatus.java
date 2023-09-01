7b7794c6cb95ece092a8c2a38e0d3e525d856be0
liviaaurich <liviaaurich2@gmail.com>
2023-08-31 23:06:12 -0300

    Mapeamento das Entidades

---
+++ src/main/java/br/com/reclamei/company/core/enumerator/ReclamationStatus.java
@@ -0,0 +1,11 @@
+package br.com.reclamei.company.core.enumerator;
+
+public enum ReclamationStatus {
+    OPEN,
+    REJECTED,
+    IN_ANALYSIS,
+    UNIDENTIFIED,
+    NO_FORECAST,
+    FORECAST,
+    RESOLVED
+}
